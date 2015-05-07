e4827ab6b9552196353f99216c676b9b4b8a1e52
Eli McMakin <elimc184@outlook.com>
2015-05-07 14:39:04 -0500

    Removes any final calls to composer in provision.sh

--- provision/provision.sh
+++ provision/provision.sh
@@ -386,12 +386,12 @@ if [[ $ping_result == "Connected" ]]; then
 		echo -e "\nDownloading wp-cli, see http://wp-cli.org"
 		git clone https://github.com/wp-cli/wp-cli.git /srv/www/wp-cli
 		cd /srv/www/wp-cli
-		composer install
+#		composer install
 	else
 		echo -e "\nUpdating wp-cli..."
 		cd /srv/www/wp-cli
 		git pull --rebase origin master
-		composer update
+#		composer update
 	fi
 	# Link `wp` to the `/usr/local/bin` directory
 	ln -sf /srv/www/wp-cli/bin/wp /usr/local/bin/wp
