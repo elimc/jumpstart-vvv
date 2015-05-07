f0d577b752542db40d495e1a942f9614a9fb5132
Eli McMakin <elimc184@outlook.com>
2015-05-07 14:29:18 -0500

    Removes opcache code in provision.sh

--- provision/provision.sh
+++ provision/provision.sh
@@ -411,15 +411,15 @@ if [[ $ping_result == "Connected" ]]; then
 
 	# Checkout Opcache Status to provide a dashboard for viewing statistics
 	# about PHP's built in opcache.
-	if [[ ! -d /srv/www/default/opcache-status ]]; then
-		echo -e "\nDownloading Opcache Status, see https://github.com/rlerdorf/opcache-status/"
-		cd /srv/www/default
-		git clone https://github.com/rlerdorf/opcache-status.git opcache-status
-	else
-		echo -e "\nUpdating Opcache Status"
-		cd /srv/www/default/opcache-status
-		git pull --rebase origin master
-	fi
+#	if [[ ! -d /srv/www/default/opcache-status ]]; then
+#		echo -e "\nDownloading Opcache Status, see https://github.com/rlerdorf/opcache-status/"
+#		cd /srv/www/default
+#		git clone https://github.com/rlerdorf/opcache-status.git opcache-status
+#	else
+#		echo -e "\nUpdating Opcache Status"
+#		cd /srv/www/default/opcache-status
+#		git pull --rebase origin master
+#	fi
 
 	# Webgrind install (for viewing callgrind/cachegrind files produced by
 	# xdebug profiler)
