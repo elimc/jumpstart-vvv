56921890ddafb079f08f1c7f9a615c60bc43ada2
Eli McMakin <elimc184@outlook.com>
2015-05-12 18:58:45 -0500

    Adds documentation to init.sql.

--- database/init.sql
+++ database/init.sql
@@ -1,8 +1,8 @@
 # We include default installations of WordPress with this Vagrant setup.
 # In order for that to respond properly, default databases should be
 # available for use.
-CREATE DATABASE IF NOT EXISTS `wordpress_default`;
-GRANT ALL PRIVILEGES ON `wordpress_default`.* TO 'wp'@'localhost' IDENTIFIED BY 'wp';
+-- CREATE DATABASE IF NOT EXISTS `wordpress_default`;
+-- GRANT ALL PRIVILEGES ON `wordpress_default`.* TO 'wp'@'localhost' IDENTIFIED BY 'wp';
 -- CREATE DATABASE IF NOT EXISTS `wordpress_trunk`;
 -- GRANT ALL PRIVILEGES ON `wordpress_trunk`.* TO 'wp'@'localhost' IDENTIFIED BY 'wp';
 -- CREATE DATABASE IF NOT EXISTS `wordpress_develop`;
@@ -10,6 +10,7 @@ GRANT ALL PRIVILEGES ON `wordpress_default`.* TO 'wp'@'localhost' IDENTIFIED BY 
 -- CREATE DATABASE IF NOT EXISTS `wordpress_unit_tests`;
 -- GRANT ALL PRIVILEGES ON `wordpress_unit_tests`.* TO 'wp'@'localhost' IDENTIFIED BY 'wp';
 
+# Create DB and grant priviledges to our users.
 CREATE DATABASE IF NOT EXISTS wordpress_default;
 GRANT ALL PRIVILEGES ON wordpress_default.* TO 'wp'@'localhost' IDENTIFIED BY 'wp';
 FLUSH PRIVILEGES;
