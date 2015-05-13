6fc9827af535bb63aa3d76db90595452b134deca
Eli McMakin <elimc184@outlook.com>
2015-05-12 19:10:14 -0500

    Remove vestigial wp install code

--- provision/provision.sh
+++ provision/provision.sh
@@ -420,33 +420,6 @@ PHP
         fi
     fi
 
-	# Install and configure the latest stable version of WordPress
-#	if [[ ! -d /srv/www/wordpress-default ]]; then
-#		echo "Downloading WordPress Stable, see http://wordpress.org/"
-#		cd /srv/www/
-#		curl -L -O https://wordpress.org/latest.tar.gz
-#		tar -xvf latest.tar.gz
-#		mv wordpress wordpress-default
-#		rm latest.tar.gz
-#		cd /srv/www/wordpress-default
-#		echo "Configuring WordPress Stable..."
-#		wp core config --dbname=wordpress_default --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
-#// Match any requests made via xip.io.
-#if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(local.wordpress.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
-#	define( 'WP_HOME', 'http://' . \$_SERVER['HTTP_HOST'] );
-#	define( 'WP_SITEURL', 'http://' . \$_SERVER['HTTP_HOST'] );
-#}
-#
-#define( 'WP_DEBUG', true );
-#PHP
-#		echo "Installing WordPress Stable..."
-#		wp core install --url=local.wordpress.dev --quiet --title="Local WordPress Dev" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
-#	else
-#		echo "Updating WordPress Stable..."
-#		cd /srv/www/wordpress-default
-#		wp core upgrade
-#	fi
-
 	# Download phpMyAdmin
 	if [[ ! -d /srv/www/default/database-admin ]]; then
 		echo "Downloading phpMyAdmin 4.2.13.1..."
